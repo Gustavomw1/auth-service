0500409ba39fb83d5e35a3661bf00db92ed11365
Gustavo <gustavo303a@gmail.com>
2025-06-13 16:56:11 -0300

    Fazendo as requisições.

---
+++ src/main/java/com/ecommerce/demo/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.ecommerce.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.demo.entity.User;
+import com.ecommerce.demo.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.login(user.getUsername(), user.getPassword());
+    }
+}
---
+++ src/main/java/com/ecommerce/demo/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.ecommerce.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.demo.entity.Product;
+import com.ecommerce.demo.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> listAll() {
+        return productService.listAll();
+    }
+
+    @PostMapping
+    public Product add(@RequestBody Product product) {
+        return productService.save(product);
+    }
+}
